78199fd2efd3d6a1d0ed55fd2a0227d5d2445b70
pabloram232 <pablo.ram232@gmail.com>
2016-11-07 23:32:55 -0600

    Add files via upload

---
+++ spring/concurso/src/main/java/concursantes/Saxofon.java
@@ -0,0 +1,15 @@
+package concursantes; 
+
+import org.springframework.stereotype.Component; 
+
+@Component
+
+public class Saxofon implements Instrumento {
+	
+	public void tocar() { 
+		
+		System.out.println("Tuu tuu tuu tuu..."); 
+		
+	} 
+	
+}
